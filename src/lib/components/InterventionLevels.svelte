cac6a98d0704e9d3383cfcfb31273a737fa8473e
Ryan Marx <ryan.marx@pulsarinformatics.com>
2025-06-24 23:16:17 -0500

    various text updates

--- src/lib/components/InterventionLevels.svelte
+++ src/lib/components/InterventionLevels.svelte
@@ -1,66 +1,69 @@
 <script lang="ts">
-	let {
-		selectedValue = $bindable(),
-		name,
-		category,
-		onValueChange,
-	}: {
-		selectedValue: string;
-		name: string;
-		category: string;
-		onValueChange?: () => void | Promise<void>;
-	} = $props();
+  let {
+    selectedValue = $bindable(),
+    name,
+    category,
+    onValueChange,
+  }: {
+    selectedValue: string;
+    name: string;
+    category: string;
+    onValueChange?: () => void | Promise<void>;
+  } = $props();
 
-	const levels = ['10%', '50%', '75%'];
+  const levels = ['10%', '50%', '75%'];
 </script>
 
 <ul class="radios" style:--total={levels.length}>
-	{#each levels as value}
-		<li>
-			<label class="radio">
-				<input
-					class="sr-only"
-					type="radio"
-					value="{category}-{value}"
-					{name}
-					bind:group={selectedValue}
-					onchange={onValueChange}
-				/>
-				{value}
-			</label>
-		</li>
-	{/each}
+  {#each levels as value}
+    <li>
+      <label class="radio">
+        <input
+          class="sr-only"
+          type="radio"
+          value="{category}-{value}"
+          {name}
+          bind:group={selectedValue}
+          onchange={onValueChange}
+        />
+        <spanc class="font-bold">
+          {value}
+        </spanc>
+        receive intervention
+      </label>
+    </li>
+  {/each}
 </ul>
 
 <style lang="postcss">
-	.radios {
-		display: grid;
-		grid-template-columns: repeat(var(--total), minmax(1px, 1fr));
-	}
+  .radios {
+    display: grid;
+    grid-template-columns: repeat(var(--total), minmax(1px, 1fr));
+  }
 
-	li:not(:first-of-type) .radio {
-		border-left: none;
-	}
+  li:not(:first-of-type) .radio {
+    border-left: none;
+  }
 
-	.radio {
-		display: block;
-		border: 1px solid #ddd;
-		padding: calc(2 * var(--spacing)) calc(4 * var(--spacing));
-		cursor: pointer;
-		text-align: center;
-		position: relative;
+  .radio {
+    display: block;
+    border: 1px solid #ddd;
+    padding: calc(2 * var(--spacing)) calc(4 * var(--spacing));
+    cursor: pointer;
+    text-align: center;
+    position: relative;
 
-		&:hover {
-			background-color: #ddd;
-		}
+    &:hover {
+      background-color: #ddd;
+    }
 
-		&:has(input:checked) {
-			font-weight: bold;
-			border-left: unset;
-			border-color: var(--color-future-ed-dark-blue);
-			border-left: 1px solid var(--color-future-ed-dark-blue);
+    &:has(input:checked) {
+      font-weight: bold;
+      border-left: unset;
+      border-color: var(--color-future-ed-dark-blue);
+      border-left: 1px solid var(--color-future-ed-dark-blue);
 
-			background: var(--color-future-ed-medium-blue);
-		}
-	}
+      background: var(--color-future-ed-medium-blue);
+    }
+  }
 </style>
--- src/lib/components/SelectDemographics.svelte
+++ src/lib/components/SelectDemographics.svelte
@@ -1,46 +1,46 @@
 <script lang="ts">
-	import RadioGroup, { type RadioItem } from '$lib/components/RadioGroup.svelte';
+  import RadioGroup, { type RadioItem } from '$lib/components/RadioGroup.svelte';
 
-	const RACES: RadioItem[] = [
-		{ value: 'black', label: 'Black' },
-		{ value: 'white', label: 'White' },
-		{ value: 'hispanic', label: 'Hispanic' },
-	] as const;
+  const RACES: RadioItem[] = [
+    { value: 'black', label: 'Black' },
+    { value: 'white', label: 'White' },
+    { value: 'hispanic', label: 'Hispanic' },
+  ] as const;
 
-	let {
-		race = $bindable('black'),
-		income = $bindable(1),
-		onchange,
-	}: {
-		race: Race;
-		income: Income;
-		onchange: () => void | Promise<void>;
-	} = $props();
+  let {
+    race = $bindable('black'),
+    income = $bindable(1),
+    onchange,
+  }: {
+    race: Race;
+    income: Income;
+    onchange: () => void | Promise<void>;
+  } = $props();
 
-	const INCOMES: RadioItem[] = [
-		{ label: 'Low income: $32,000 for a family of four', value: 1 },
-		{
-			label: 'Moderate income: $32,000 to $64,000.',
-			value: 2,
-		},
-		{ label: 'Middle income and above: More than $64,000', value: 3 },
-	] as const;
+  const INCOMES: RadioItem[] = [
+    { label: 'Low income: $32,000 for a family of four', value: 1 },
+    {
+      label: 'Moderate income: $32,000 to $64,000 for a family of four',
+      value: 2,
+    },
+    { label: 'Middle income and above: More than $64,000 for a family of four', value: 3 },
+  ] as const;
 </script>
 
 <section class="bg-container">
-	<h3 class="future-ed-header h1" style:grid-column="1" data-counter="2">Select demographics</h3>
-	<div>
-		<h4 class="label">Race/ethnicity</h4>
-		<RadioGroup items={RACES} bind:selectedValue={race} name="race" {onchange} />
-	</div>
-	<div>
-		<h4 class="label">Parent income in childhood</h4>
-		<RadioGroup items={INCOMES} bind:selectedValue={income} name="income" {onchange} />
-	</div>
+  <h3 class="future-ed-header h1" style:grid-column="1" data-counter="2">Select population</h3>
+  <div>
+    <h4 class="label">Race/ethnicity</h4>
+    <RadioGroup items={RACES} bind:selectedValue={race} name="race" {onchange} />
+  </div>
+  <div>
+    <h4 class="label">Parent income in childhood</h4>
+    <RadioGroup items={INCOMES} bind:selectedValue={income} name="income" {onchange} />
+  </div>
 </section>
 
 <style lang="postcss">
-	div {
-		flex: 1 1 max-content;
-	}
+  div {
+    flex: 1 1 max-content;
+  }
 </style>
