af2cd4c6c5936e3914ec0db35e567a8d2dc1fdad
Ryan Marx <ryan.marx@pulsarinformatics.com>
2025-06-25 02:15:55 -0500

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,68 @@
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   build: {
+//     emptyOutDir: true,
+//     lib: {
+//       entry: 'src/main.ts',
+//       name: 'SvelteViteApp',
+//       fileName: (format) => `svelte-vite-app.${format}.js`,
+//       formats: ['es', 'umd'],
+//     },
+//     rollupOptions: {
+//       output: {
+//         inlineDynamicImports: true,
+//       },
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//       $lib: '/src/lib',
+//     },
+//   },
+//   plugins: [
+//     tailwindcss(),
+//     svelte(),
+//     // svelte({
+//     //   include: /\.wc\.svelte$/ as any,
+//     //   compilerOptions: {
+//     //     customElement: true,
+//     //     css: 'injected',
+//     //   },
+//     // }),
+//     // svelte({
+//     //   exclude: /\.wc\.svelte$/ as any,
+//     //   compilerOptions: {
+//     //     customElement: false,
+//     //     css: 'injected',
+//     //   },
+//     // }),
+//   ],
+// });
+
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    lib: {
-      entry: 'src/main.ts',
-      name: 'SvelteViteApp',
-      fileName: (format) => `svelte-vite-app.${format}.js`,
-      formats: ['es', 'umd'],
+  plugins: [tailwindcss(), svelte()],
+  base: '/future-ed-mobility/',
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+      $: path.resolve('./src'),
     },
+  },
+  build: {
+    outDir: './build',
     rollupOptions: {
       output: {
-        inlineDynamicImports: true,
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
       },
     },
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-      $lib: '/src/lib',
-    },
-  },
-  plugins: [
-    tailwindcss(),
-    svelte(),
-    // svelte({
-    //   include: /\.wc\.svelte$/ as any,
-    //   compilerOptions: {
-    //     customElement: true,
-    //     css: 'injected',
-    //   },
-    // }),
-    // svelte({
-    //   exclude: /\.wc\.svelte$/ as any,
-    //   compilerOptions: {
-    //     customElement: false,
-    //     css: 'injected',
-    //   },
-    // }),
-  ],
 });
