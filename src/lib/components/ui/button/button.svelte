2c614d5735cd52c039e2f599a120f55fd63897fb
Ryan Marx <ryan.marx@pulsarinformatics.com>
2025-06-25 01:41:41 -0500

    fix buttons

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -12,9 +12,9 @@
         destructive:
           'bg-destructive shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white',
         outline:
-          'font-normal bg-[transparent] border-(--color-future-ed-dark-blue) text-(--color-future-ed-dark-blue) shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-(--color-future-ed-dark-blue) dark:hover:bg-input/50 border',
+          'border-solid font-normal bg-[transparent] border-(--color-future-ed-dark-blue) text-(--color-future-ed-dark-blue) shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-(--color-future-ed-dark-blue) dark:hover:bg-input/50 border',
         'outline-selected':
-          'font-bold bg-background border-(--color-future-ed-dark-blue) text-(--color-future-ed-dark-blue) shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-(--color-future-ed-dark-blue) dark:hover:bg-input/50 border',
+          'border-solid  font-bold bg-background border-(--color-future-ed-dark-blue) text-(--color-future-ed-dark-blue) shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-(--color-future-ed-dark-blue) dark:hover:bg-input/50 border',
         secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
