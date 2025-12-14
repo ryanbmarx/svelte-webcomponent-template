b8b850004914f9b164556ee431b9f3862d967179
Ryan Marx <ryan.marx@pulsarinformatics.com>
2025-12-13 20:15:35 -0600

    Clean up and rename main app file

---
+++ src/App.wc.svelte
@@ -0,0 +1,17 @@
+<!-- <svelte:options customElement="future-ed-mobility-explorer" /> -->
+
+<script lang="ts">
+ 
+</script>
+
+<!-- <svelte:element this={'style'}>{styles}</svelte:element> -->
+<main>
+  
+  <!-- APP HERE -->
+</main>
+
+<!-- <Toaster /> -->
+
+<style lang="postcss">
+
+</style>
